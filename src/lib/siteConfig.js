c740f13c4ac496505e914cb810da91bdbb886647
Martin Grubinger <martin.grubinger@gmail.com>
2023-01-09 21:48:48 +0100

    path fuckup

--- src/lib/siteConfig.js
+++ src/lib/siteConfig.js
@@ -4,7 +4,7 @@ export const GH_USER_REPO = 'mgrubinger/blog'; // used for pulling github issues
 export const SITE_TITLE = 'grooovinger';
 export const SITE_DESCRIPTION = "Hi, I'm Martin Grubinger, builder of web-things.";
 export const DEFAULT_OG_IMAGE =
-	{SITE_URL} + '/ogimage-1.png';
+	'https://grooovinger.com/ogimage-1.png';
 export const MY_TWITTER_HANDLE = 'mgrubinger';
 export const MY_MASTODON_HANDLE = '@grooovinger@mastodon.social';
 export const MY_MASTODON_URL = 'https://mastodon.social/@grooovinger';
