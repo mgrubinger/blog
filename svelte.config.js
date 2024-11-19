715382d0c4ee83f93047da251214b34a4c4ea447
Martin Grubinger <martin@Air-von-Martin.fritz.box>
2024-11-19 20:58:19 +0100

    switch to adapter-node

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 // import adapter from '@sveltejs/adapter-static';
 import { mdsvex } from 'mdsvex';
 import remarkGithub from 'remark-github';
