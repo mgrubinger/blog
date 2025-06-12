c8433414d484d8f770f41cedea6a5fadd51ecd7b
Martin Grubinger <martin.grubinger@geizhals.at>
2025-06-12 08:14:13 +0200

    no trailing slashes, fixes canonical

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
-import svelte from '@astrojs/svelte';
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://grooovinger.com',
-    integrations: [mdx(), sitemap(), svelte()],
+  site: "https://grooovinger.com",
+  integrations: [mdx(), sitemap(), svelte()],
+  trailingSlash: "never",
 });
